cc5cdda9a03c4305bb6f98b0937e55e68fdfa4d4
Daniel Evans <migetman9@gmail.com>
2017-11-28 12:31:58 -0800

    add signup function

--- soda_ledger/site_sections/soda.py
+++ soda_ledger/site_sections/soda.py
@@ -22,3 +22,30 @@ class Root:
 
     def manage(self, session):
         return ''
+
+    @unrestricted
+    def signup(self, session,  password='', message='', **params):
+        user = session.user(params, checkgroups=User.all_checkgroups, bools=User.all_bools)
+        if user.is_new:
+            message = check(user)
+            if not message and 'first_name' in params:
+                session.add(user)
+                session.commit()
+                password = password if password else genpasswd()
+                admin_params = {
+                    'access': '{}'.format(c.SODA),
+                    'user_id': user.id,
+                    'password': password
+                }
+                account = session.admin_account(admin_params, checkgroups=['access'])
+                account.hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+                message = check(account)
+                if not message:
+                    message = 'Account Created'
+                    account.user = user
+                    session.add(account)
+                raise HTTPRedirect('index?message={}', message)
+            return {
+                'user': user
+            }
+        raise HTTPRedirect('index?message={}', message)
